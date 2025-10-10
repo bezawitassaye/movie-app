2c7d9af7bd84ec1eefbe056b42affa5e8eb3b69c
bezawitassaye <beza1221love@gmail.com>
2025-10-11 00:38:26 +0300

    make tailwind css works

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View className="flex-1 justify-center items-center "
     >
-      <Text>welcome</Text>
+      <Text className="text-blue-600 text-6xl">welcome</Text>
     </View>
   );
 }
