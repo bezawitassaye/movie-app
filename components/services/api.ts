491147f8fe52849ede56224e248d7b39a824d120
bezawitassaye <beza1221love@gmail.com>
2025-10-11 14:27:35 +0300

    include api

---
+++ components/services/api.ts
@@ -0,0 +1,13 @@
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjRhMDdhMjk1YTZkNTA4YzViNDZmZTBjOTIzZGE2MCIsIm5iZiI6MTc2MDE4MTI0NC43NTgwMDAxLCJzdWIiOiI2OGVhM2JmY2RhNTM3ZWVlMDQ3ODY4NzkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.doYggYPoeXQPaG75Gu21f9jjkQA3HhIAFyHKoWhoLbo'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
