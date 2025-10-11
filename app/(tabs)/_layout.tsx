d52335e7ebbc28bc2ab31912982fa0012707ae9e
bezawitassaye <beza1221love@gmail.com>
2025-10-11 04:03:06 +0300

    FINISH THE FIRST TAB STYLE

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
+import { icons } from '@/constants/icons';
+import { images } from '@/constants/images';
 import { Tabs } from 'expo-router';
 import React from 'react';
-
+import { Image, ImageBackground, Text } from 'react-native';
 const _layout = () => {
   return (
     <Tabs>
@@ -9,6 +11,19 @@ const _layout = () => {
         options={{
           title: 'Home',
           headerShown: false,
+          tabBarIcon:({focused})=>(
+            <>
+            <ImageBackground
+            source={images.highlight}
+            className='felx flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center
+            items-center rounded-full overflow-hidden'>
+               <Image source={icons.home}
+               tintColor={"#151312"} className='size-5'/>
+               <Text className='text-secondary text-base font-semibold ml-2'>Home</Text>
+
+            </ImageBackground>
+            </>
+          )
         }}
       />
       <Tabs.Screen
