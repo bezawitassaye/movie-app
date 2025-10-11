3cd7610d5b499084f41c7b6ec23f2ef99f028129
bezawitassaye <beza1221love@gmail.com>
2025-10-11 20:44:56 +0300

    display movies

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { Link } from "expo-router";
+import { Image, Text, TouchableOpacity } from "react-native";
 
-const MovieCard = () => {
-  return (
-    <View>
-      <Text>MovieCard</Text>
-    </View>
-  )
+interface Movie {
+  id: number;
+  poster_path?: string;
+  title?: string;
+  vote_average?: number;
+  release_date?: string;
 }
 
-export default MovieCard
+const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie) => {
+  return (
+    <Link href={`/movie/${id}`} asChild>
+      <TouchableOpacity className="w-[30%]">
+        <Image
+          source={{
+            uri: poster_path
+              ? `https://image.tmdb.org/t/p/w500${poster_path}`
+              : "https://placehold.co/600x400/1a1a1a/ffffff.png",
+          }}
+          className="w-full h-52 rounded-lg"
+          resizeMode="cover"
+        />
+
+        <Text className="text-sm font-bold text-white mt-2" >{title}</Text>
+      </TouchableOpacity>
+    </Link>
+  );
+};
+
+export default MovieCard;
