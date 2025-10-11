c15040fcdcf6dae3fa4ebe94718ed1a61c32b06d
bezawitassaye <beza1221love@gmail.com>
2025-10-11 13:51:38 +0300

    finish search ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,15 +8,16 @@ export default function Index() {
     <View className="flex-1 bg-primary "
     >
       <Image source={images.bg} className="absolute w-full z-0"/>
-      <ScrollView className="flex-1 px-5 mt-20 mb-5 mx-auto"
+      <ScrollView className="flex-1 px-5 "
       showsVerticalScrollIndicator={false} contentContainerStyle={{
         minHeight:"100%", paddingBottom:10
       }}>
-        <Image source={icons.logo} className="w-12 h-10"/>
+        <Image source={icons.logo} className="w-12 h-10
+        mt-20 mb-5 mx-auto"/>
      
-      <View>
+      <View className="flex-1 mt-5 ">
         <SearchBar/>
-      </View>
+      </View >
       </ScrollView>
     </View>
   );
