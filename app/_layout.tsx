a7718b02a76ded5a1d5654e000be96a9787fca0d
bezawitassaye <beza1221love@gmail.com>
2025-10-12 05:24:12 +0300

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,22 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { StatusBar } from 'react-native';
 import './global.css';
 
 
 export default function RootLayout() {
-  return <Stack>
+  return <>
+  <StatusBar hidden={true}/>
+   <Stack>
+    
 
     <Stack.Screen name="(tabs)" options={{headerShown: false}}>
+    
     </Stack.Screen>
      
      <Stack.Screen name="movies/[id]" options={{headerShown: false}}>
      </Stack.Screen>
 
   </Stack>
+
+  </>
 }
